ff44076c679563e65b50278a3541113a583a8fc6
wilson <wilsongayo@golden-success.com>
2024-03-24 15:29:36 +0800

    Updated branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,17 +44,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Develop by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://wilschoy78.github.io/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Wilson Gayo</span>
       </a>
     </span>
   );
